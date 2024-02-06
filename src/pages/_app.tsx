dfa26c89ada08c29d65b43c433bff6c9eef1723d
Patrickhuiwork <patrick.hui.work@gmail.com>
2024-02-05 22:13:20 -0800

    asdadsasdasdsadsads

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link href="https://api.fontshare.com/v2/css?f[]=general-sans@1&display=swap" rel="stylesheet" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,20 @@
 import Image from "next/image"
 import  NavBar  from "../components/NavBar"
+import styles from "../styles/Home.module.css"
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24`}
-    >
+    <>
       <NavBar />
-    </main>
+      <div className={styles.main}>
+        <div className={styles.maintext}>
+          <h2>Don’t Know The Weather Use <strong>SKY-CAST</strong></h2>
+          <ol>
+            <li>Search for a location</li>
+            <li>Find out the current weather situation, tomorrow’s forecast, or a 6-day forecast</li>
+          </ol>
+        </div>
+      </div>
+    </>
   );
 }
